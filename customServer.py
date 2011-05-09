d9a754f8def9da94da0c22b14ac54b1090f90e86
Christopher <chris@immersivelabs.com>
2011-05-08 20:56:36 -0600

    example of a custom

---
+++ customServer.py
@@ -0,0 +1,23 @@
+__author__      = "Christopher Piekarski"
+__email__       = "polo1065@gmail.com"
+__copyright__= " Copyright, 2011"
+__version__     = "1.0.0"
+
+
+import jsonSocket
+
+class MyServer(jsonSocket.ThreadedServer):
+    def __init__(self):
+        super(MyServer, self).__init__()
+        self.timeout = 2.0
+    
+    def _processMessage(self, obj):
+        """ virtual method """
+        if obj != '':
+            if obj['message'] == "new connection":
+                pass
+    
+    
+if __name__ == "__main__":
+    c = MyServer()
+    c.start()
