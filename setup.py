c9640ec9bb970bd77adf669c73b64c45abe91c3e
Christopher Piekarski <christopherpiekarski@yahoo.com>
2011-08-24 08:27:50 -0600

    new setup file so that sdist packages can be uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='JsonSocket',
+      version='1.0',
+      description='Python JSON Server & Client',
+      author='Christopher Piekarski',
+      author_email='polo1065@gmail.com',
+      keywords=['json','socket','server','client'],
+      classifiers=[
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2.7',
+                   'Operating System :: OS Independent',
+                   ],
+      url='http://cpiekarski.com/2011/05/09/super-easy-python-json-client-server/',
+      py_modules=['jsonSocket', 'threadedServer','customServer'],
+     )
