560b5fb76678b995c29c9adfbbff4c22a59247cf
Christopher Piekarski <christopherpiekarski@yahoo.com>
2011-09-25 10:04:32 -0600

    added logging, main test section, and better example

--- customServer.py
+++ customServer.py
@@ -5,19 +5,35 @@ __version__     = "1.0.0"
 
 
 import threadedServer
+import jsonSocket
+import logging
+
+logger = logging.getLogger("jsonSocket.customServer")
 
 class MyServer(threadedServer.ThreadedServer):
     def __init__(self):
         super(MyServer, self).__init__()
         self.timeout = 2.0
+        logger.warning("MyServer class in customServer is for example purposes only.")
     
     def _processMessage(self, obj):
         """ virtual method """
         if obj != '':
             if obj['message'] == "new connection":
-                pass
+                logger.info("new connection.")
                
     
 if __name__ == "__main__":
-    c = MyServer()
-    c.start()
+    import time
+    server = MyServer()
+    server.start()
+    
+    time.sleep(1)
+    
+    client = jsonSocket.JsonClient()
+    client.connect()
+    
+    time.sleep(1)
+    
+    client.close()
+    server.stop()
