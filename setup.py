ad93add817a3f784fa41553068e162ba70eeb0b3
Christopher Piekarski <chris@cpiekarski.com>
2011-12-05 00:40:42 -0700

    wip change on setup script

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 
 from distutils.core import setup
 
-setup(name='JsonSocket',
+setup(name='jsocket',
       version='1.3',
       description='Python JSON Server & Client',
       author='Christopher Piekarski',
       author_email='polo1065@gmail.com',
       keywords=['json','socket','server','client'],
+      packages=['jsocket'],
+      provides=['jsocket'],
       classifiers=[
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: Apache Software License',
                    'Programming Language :: Python :: 2.7',
                    'Operating System :: OS Independent',
                    ],
-      url='http://cpiekarski.com/2011/05/09/super-easy-python-json-client-server/',
-      py_modules=['jsocket', 'tserver','example_servers'],
+      url='http://cpiekarski.com/2011/05/09/super-easy-python-json-client-server/'
      )
