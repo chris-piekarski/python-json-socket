36555b221fddc32ff90506c5e530724f6907e5a1
Christopher Piekarski <chris@cpiekarski.com>
2019-02-11 11:10:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='jsocket3',
+setup(name='jsocket',
       version='1.7',
       description='Python JSON Server & Client',
       author='Christopher Piekarski',
