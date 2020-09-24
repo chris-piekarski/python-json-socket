480fcb9c915b5a0eeea46a54657f28c2582e5ee2
Chris Piekarski <chris@cpiekarski.com>
2020-09-23 18:34:40 -0600

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, Extension
 
 setup(name='jsocket',
-      version='1.8.1',
+      version='1.8.2',
       description='Python JSON Server & Client',
       author='Christopher Piekarski',
       author_email='chris@cpiekarski.com',
