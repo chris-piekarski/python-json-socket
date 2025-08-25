fb24526634190dd00263c0deb8b445d2e1bba247
Chris Piekarski <chris@cpiekarski.com>
2025-08-25 12:27:18 -0600

    bump version

--- jsocket/jsocket_base.py
+++ jsocket/jsocket_base.py
@@ -19,7 +19,7 @@ __copyright__= """
     See the License for the specific language governing permissions and
     limitations under the License.
 """
-__version__  = "1.0.2"
+__version__  = "1.0.3"
 
 import json
 import socket
