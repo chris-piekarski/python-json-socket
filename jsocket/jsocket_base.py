efba8b6b8726ed614cd9d4f0300dfc91c0ec5545
Christopher Piekarski <chris@cpiekarski.com>
2026-02-10 08:40:03 -0700

    fix: merge pr (#26)
    * fix: address possible desync framing scenarios
    
    * ci: fix makefile and lint issues
    
    * ci: actions lint error
    
    * ci: additional unit and behave coverage
    fix: break out socket timeouts
    fix: reduce possible fd leak
    
    * docs: update for dual timeout options
    
    * ci: legacy...

--- jsocket/jsocket_base.py
+++ jsocket/jsocket_base.py
@@ -337,7 +337,7 @@ class JsonClient(JsonSocket):
     def __init__(self, address='127.0.0.1', port=5489, timeout=2.0, recv_timeout=None):
         super().__init__(address, port, timeout=timeout, recv_timeout=recv_timeout)
         if self.socket is not None:
-            self.socket.settimeout(self._timeout)
+            self.socket.settimeout(self._recv_timeout)
 
     def connect(self):
         """Attempt to connect to the server up to 10 times with backoff."""
@@ -350,7 +350,7 @@ class JsonClient(JsonSocket):
                 # Recreate the socket to avoid retrying on a potentially bad fd.
                 self._close_socket()
                 self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.socket.settimeout(self._timeout)
+                self.socket.settimeout(self._recv_timeout)
                 self.conn = self.socket
                 logger.debug("recreated socket for retry %d to %s:%s", attempt, self.address, self.port)
                 time.sleep(3)
